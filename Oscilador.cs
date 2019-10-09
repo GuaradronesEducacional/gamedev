23af53f0f1fb5f6e6710b4ef8ebc6dd192525f85
guaradrones <54602036+guaradrones@users.noreply.github.com>
2019-10-09 13:32:33 -0300

    Add files via upload

---
+++ Oscilador.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscilador : MonoBehaviour {
+
+    [SerializeField] Vector3 vetorDeMovimento = new Vector3(10f, 10f, 10f);
+    [SerializeField] float periodo = 2f;
+
+    float fatorDeMovimento; // 0 para sem movimento 1 para movimento total
+    Vector3 startingPos;
+
+    // Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+       
+        float ciclos = Time.time / periodo; //crescem continuamente de zero
+
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(ciclos * tau);
+
+        fatorDeMovimento = rawSinWave / 2f + 0.5f;
+        Vector3 offset = fatorDeMovimento * vetorDeMovimento;
+        transform.position = startingPos + offset;
+	}
+}
